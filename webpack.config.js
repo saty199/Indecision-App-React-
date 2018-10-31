ddfe4e3963ebc3c597189ea68d3f986d4aceb71b
Satyam Agrawal <kansalsatyam1995@gmail.com>
2018-11-01 02:38:20 +0530

    webpack usage and babel setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path= require('path');
+console.log(path.join(__dirname, 'public'));
+module.exports={
+    entry:'./src/app.js',
+    output:{
+        path: path.join(__dirname, 'public'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[{
+            loader:'babel-loader',
+            test: /\.js$/,
+            exclude:/node_modules/
+        }]
+    }
+}
